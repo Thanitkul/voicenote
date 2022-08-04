aea3b655741ff24677726145c30f61239ba37326
SEN-SENSATION <prawich.th@gmail.com>
2022-08-04 11:32:00 +0700

    SCSS setup + Redux Setup

---
+++ student/src/context/authSlice.js
@@ -0,0 +1,25 @@
+const initialState = {
+  isAuthenticated: false,
+  name: "",
+  email: "",
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.isAuthenticated = true;
+
+      state.name = action.payload.name;
+      state.email = action.payload.email;
+    },
+    logout: (state) => {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const authActions = authSlice.actions;
+
+export default authSlice;
--- student/src/index.jsx
+++ student/src/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import store from "./context/store";
+import { Provider } from "react-redux";
 
 import App from "./App";
+
+import "./sass/main.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </>
 );
