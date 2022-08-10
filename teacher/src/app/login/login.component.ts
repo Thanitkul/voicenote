7e07e36d6453fd3a72459a914c5c30b19d6249be
Trollgodlol <86240535+Trollgodlol@users.noreply.github.com>
2022-08-10 15:17:00 +0700

    add session storage

--- teacher/src/app/login/login.component.ts
+++ teacher/src/app/login/login.component.ts
@@ -7,12 +7,24 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  insertInfo(){
+    let email = sessionStorage.getItem('email');
+    let password = sessionStorage.getItem('password');
+
+  }
 
   getinfo(){
     let email = (<HTMLInputElement>document.getElementById("email")).value;
     let password = (<HTMLInputElement>document.getElementById("password")).value;
-    console.log(email);
-    console.log(password);
+    let rem = (<HTMLInputElement>document.getElementById("rem")).checked;
+
+    if(rem == true){
+      sessionStorage.setItem('email',email);
+      sessionStorage.setItem('password',password);
+    }
+    else{
+      console.log("not checked");      
+    } 
   }
     
   
