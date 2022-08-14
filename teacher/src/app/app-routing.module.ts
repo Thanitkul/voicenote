9b27848fb0d07dcebe6c8ed10ab857f580c4abff
kasidismana <kasidismana@gmail.com>
2022-08-15 00:42:13 +0700

    create routing

--- teacher/src/app/app-routing.module.ts
+++ teacher/src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountCreationComponent } from './account-creation/account-creation.component';
+import { CourseComponent } from './course/course.component';
+import { LoginComponent } from './login/login.component';
 import { SpeakComponent } from './speak/speak.component';
 
 const routes: Routes = [
-  { path: 'speak', component: SpeakComponent}
+    { path: '', component: LoginComponent },
+    { path: 'speak', component: SpeakComponent },
+    { path: 'course', component: CourseComponent },
+    { path: 'register', component: AccountCreationComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [LoginComponent, SpeakComponent, CourseComponent, AccountCreationComponent]
--- teacher/src/app/app.module.ts
+++ teacher/src/app/app.module.ts
@@ -1,31 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccountCreationComponent } from './account-creation/account-creation.component';
-import { LoginComponent } from './login/login.component';
-import { CourseComponent } from './course/course.component';
 import { TranscriptionComponent } from './transcription/transcription.component';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { FormsModule } from '@angular/forms';
-import { SpeakComponent } from './speak/speak.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    AccountCreationComponent,
-    LoginComponent,
-    CourseComponent,
-    TranscriptionComponent,
-    SpeakComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    Ng2SearchPipeModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AccountCreationComponent,
+        routingComponents
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        Ng2SearchPipeModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
+
