8e4a69ed4b0e27a1edf6964e2a90077372c905e0
Phasit Thanitkul_Kane <kanephasit@gmail.com>
2022-09-09 00:36:32 +0700

    deployed api to heroku, only database left

--- server/server.js
+++ server/server.js
@@ -26,7 +26,6 @@ console.log(process.env.DB_HOST, process.env.DB_PASSWORD)
 export const con = await mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE
 });
