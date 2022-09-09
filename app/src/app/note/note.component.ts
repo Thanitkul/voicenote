a37a14137b1749eafade2c5930373274cf08f9ae
Phasit Thanitkul_Kane <kanephasit@gmail.com>
2022-09-09 11:23:35 +0700

    note socket

--- app/src/app/note/note.component.ts
+++ app/src/app/note/note.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NoteService } from './note.service';
 
 @Component({
   selector: 'app-note',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note.component.scss']
 })
 export class NoteComponent implements OnInit {
-
-  constructor() { }
+  roomSelected: string = 'Room A'
+  message: string = ''
+  wordList: string[] = []
+  constructor(private service: NoteService) { }
 
   ngOnInit(): void {
+    this.service.socketListen('message').subscribe(
+      (response) => {
+        this.wordList.push(response)
+      })
   }
+  joinRoom(room: string) {
+    this.roomSelected = room
+    this.service.socketConnection(room)
+    this.service.getChatsByRoom(room).subscribe({
+        next: (words: any) => {
+            this.wordList = words.map(
+                (word: any) => word.message)
+        },
+        error: (error) => console.log(error)
+    })
+}
+
+sendMessage() {
+    // socket emit event message
+    this.service.socketEmit('message', {
+        room: this.roomSelected, 
+        messageText: this.message
+    })
+    this.message = ''
+}
+
+disconnect() {
+    console.log('Disconnect')
+}
 
 }
