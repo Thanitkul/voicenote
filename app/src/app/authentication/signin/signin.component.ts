80c1cb57bca2b1d5d9130c0652ab16e40516928e
Phasit Thanitkul_Kane <kanephasit@gmail.com>
2022-09-12 14:03:36 +0700

    dynamic signin with color

--- app/src/app/authentication/signin/signin.component.ts
+++ app/src/app/authentication/signin/signin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { Router } from '@angular/router';
+import { StudentComponent } from 'src/app/courses/student/student.component';
 
 @Component({
   selector: 'app-signin',
@@ -14,12 +15,19 @@ export class SigninComponent implements OnInit {
   }
   constructor(private authServ: AuthenticationService, private router: Router) { }
   route_to: string = ''
+  name: string = ''
+  
 
   ngOnInit(): void {
     if (localStorage.getItem('role') == null){
       this.router.navigate(['/authentication/landing'])
     }
     this.route_to = localStorage.getItem('role')!
+    this.name = this.route_to.toUpperCase()
+
+    if (this.route_to == "student") {
+      document.getElementsByName("who")[0].style.backgroundColor = "#594835"
+    }
 
   }  
 
