35d27a65306ec43dd4cb1086430ac0bff58e5a0c
ujbaobao9899 <ujbaobao9899@gmail.com>
2022-09-13 14:28:36 +0700

    updated

--- app/src/app/note/note.component.ts
+++ app/src/app/note/note.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { noteService } from './note.service';
+
+declare var transcript: string;
 
 @Component({
   selector: 'app-note',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note.component.scss']
 })
 export class NoteComponent implements OnInit {
+  roomSelected: string = 'Room A'
+  message: string = transcript
+  chatList: string[] = []
 
-  constructor() { }
+  constructor(private service: noteService) { }
 
   ngOnInit(): void {
+    this.service.socketListen('message').subscribe(
+      (response) => {
+          this.chatList.push(response)
+      })
   }
+  // joinRoom(room: string) {
+  //   this.roomSelected = room
+  //   this.service.socketConnection(room)
+  //   this.service.getChatsByRoom(room).subscribe({
+  //       next: (chats: any) => {
+  //           this.chatList = chats.map(
+  //               (chat: any) => chat.message)
+  //       },
+  //       error: (error) => console.log(error)
+  //   })
+// }
 
+sendMessage() {
+    // socket emit event message
+    this.service.socketEmit('message', {
+        room: this.roomSelected, 
+        messageText: this.message
+    })
+    this.message = ''
 }
+
+disconnect() {
+    console.log('Disconnect')
+}
+}
+
+
