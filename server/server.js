4b43cde26610521d31c6425cee2d9a7cce585392
Phasit Thanitkul_Kane <kanephasit@gmail.com>
2022-09-22 16:54:06 +0700

    server room, save into database, and get record data

--- server/server.js
+++ server/server.js
@@ -49,12 +49,14 @@ IO.on('connection', (socket) => {
       socket.join(room);
   });
 
-  socket.on('message', async ({ room, messageText }) => {
+  socket.on('message', async ({ room, messageText, recordingId }) => {
       // await con.query('INSERT INTO chats (message, room, created) VALUES (?, ?, ?)', [
       //     messageText, 
       //     room,
       //     new Date()
       // ])
+
+      await con.query("UPDATE recordings SET data = CONCAT(data, ?) WHERE id = ?", [messageText, recordingId]);
       
       IO.to(room).emit('message', messageText)
   });
