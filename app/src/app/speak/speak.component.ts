48e525eb4d132dca91c280fd6bda3b4b18e1934c
Phasit Thanitkul_Kane <kanephasit@gmail.com>
2022-09-25 22:45:19 +0700

    socket edit

--- app/src/app/speak/speak.component.ts
+++ app/src/app/speak/speak.component.ts
@@ -32,7 +32,7 @@ export class SpeakComponent implements OnInit {
     finalSpan: string = '';
     displayList: string[] = [];
     room : any;
-    recordingId: any = -1;
+    groupId: any = -1;
     stopModal: any;
 
     constructor(private service: speakService, private route: ActivatedRoute, private router: Router) {
@@ -49,7 +49,7 @@ export class SpeakComponent implements OnInit {
                 this.displayList.push(response)
             }
         })
-        this.recordingId = this.service.startLive(this.room).subscribe(res => { this.recordingId = res.recordingId, console.log(this.recordingId) })
+        this.groupId = this.service.startLive(this.room).subscribe(res => { this.groupId = res.groupId, console.log(this.groupId) })
         
         
     }
@@ -131,7 +131,7 @@ export class SpeakComponent implements OnInit {
                 this.service.socketEmit('message', {
                     room: this.room,
                     messageText: _transcript,
-                    recordingId: this.recordingId
+                    groupId: this.groupId
                 })
             }
 
