7c78d5aa8cd787c5e8c4e79268bced698e358208
Phasit Thanitkul_Kane <kanephasit@gmail.com>
2022-09-26 15:30:28 +0700

    archive finishes

--- app/src/app/archive/archive.component.ts
+++ app/src/app/archive/archive.component.ts
@@ -9,13 +9,16 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ArchiveComponent implements OnInit {
   groupId: any = -1;
-  data: any;
+  display: any = [];
 
   constructor(private service: ArchiveService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    let data: any;
     this.groupId = this.route.snapshot.paramMap.get('groupId')
-    this.service.getCourseData(this.groupId).subscribe((res: any) => {this.data = res, console.log(this.data)}) 
+    this.service.getCourseData(this.groupId).subscribe((res: any) => {this.display = res, console.log(this.display)}) 
+
+    console.log(this.display)
   }
 
 }
