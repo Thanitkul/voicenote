cc11f46d15a9aa01132f569abe09dfdce0278ace
kasidis-manasurangkul <84066800+kasidis-manasurangkul@users.noreply.github.com>
2022-09-27 00:48:11 +0700

    sent coursename from course to history and archive

--- app/src/app/archive/archive.component.ts
+++ app/src/app/archive/archive.component.ts
@@ -10,6 +10,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class ArchiveComponent implements OnInit {
   groupId: any = -1;
   display: any = [];
+  courseName: string | null = '';
 
   constructor(private service: ArchiveService, private route: ActivatedRoute, private router: Router) { }
 
@@ -17,6 +18,7 @@ export class ArchiveComponent implements OnInit {
     let data: any;
     this.groupId = this.route.snapshot.paramMap.get('groupId')
     this.service.getCourseData(this.groupId).subscribe((res: any) => {this.display = res, console.log(this.display)}) 
+    this.courseName = this.route.snapshot.queryParamMap.get('courseName');
 
     console.log(this.display)
   }
