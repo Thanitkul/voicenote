dda8bfa2bbb8733843b4a6c282793ce1bbb01fb1
kasidismana <kasidismana@gmail.com>
2022-10-06 17:41:41 +0700

    send courseName from course to note

--- app/src/app/note/note.component.ts
+++ app/src/app/note/note.component.ts
@@ -11,12 +11,14 @@ export class NoteComponent implements OnInit {
   groupId: any = -1;
   displayList: string[] = [];
   room: any = "";
+  courseName: any = {};
     
     constructor(private service: noteService, private router: Router, private route: ActivatedRoute) { }
 
     ngOnInit(): void {
         this.room = this.route.snapshot.paramMap.get('courseId')
         this.groupId = this.route.snapshot.paramMap.get('groupId')
+        this.courseName = this.route.snapshot.queryParamMap.get('courseName')
         this.service.getCourseData(this.groupId).subscribe((res: any) => {this.displayList = res, console.log(this.displayList)})
         this.service.socketConnection(this.room);
         this.service.socketListen('message').subscribe({
@@ -24,6 +26,7 @@ export class NoteComponent implements OnInit {
                 this.displayList.push(response)
             }
         })
+        console.log(this.courseName)
     }
 
     redirectCourse() {
