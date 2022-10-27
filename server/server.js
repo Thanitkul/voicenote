52f75fa4b3feb361909e1cc822be382211fd06f9
Phasit Thanitkul_Kane <kanephasit@gmail.com>
2022-10-27 15:10:52 +0700

    delete comma at line 34 and change port

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = require('./app')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8100
 const http = require('http')
 const server = http.createServer(app)
 const { Server } = require("socket.io")
